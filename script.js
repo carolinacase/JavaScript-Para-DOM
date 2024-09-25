16719e68193fa45c32851c963b6fd8b245056568
Carolina Case <69168196+carolinacase@users.noreply.github.com>
2024-09-25 17:22:07 -0300

    Add index.html, style.css, and script.js for JavaScript DOM project

---
+++ script.js
@@ -0,0 +1,7 @@
+//FUNCIONES DECLARATIVAS 
+function changeThisElement(element){
+    element.innerHTML = 'Hola mundo desde funcion declarativa'
+}
+
+//ARROW FUNCIONS
+const changeThisElement = element => element.innerHTML = 'Hola mundo desde funcion declarativa'
